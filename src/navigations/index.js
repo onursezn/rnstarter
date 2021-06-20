9fe50ef75c3730cbe4aad43d450dfa0652b7798b
Mustafa Onur Sezen <onursezn@gmail.com>
2021-06-20 23:25:19 +0300

    Working on navigation

---
+++ src/navigations/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const AppNavContainer = () => {
+    return (
+        <NavigationContainer>
+            <Text>Hello</Text>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavContainer;
